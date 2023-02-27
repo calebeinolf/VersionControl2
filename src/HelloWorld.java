4a87aa95a190aafbcab54d38705545b3e1a57141
FREDERICKMA21 <frederickMA21@gcc.edu>
2023-02-27 10:48:20 -0500

    Added a scanner for name

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main (String[] args ){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a name: ");
+       String name = scan.nextLine();
+        System.out.println("Your name is: " + name);
+
+
+
     }
 }
